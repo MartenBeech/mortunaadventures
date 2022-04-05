fa1de780a03e6ee92429040018b09e7f970e958d
MartenBeech <mortenbech22@hotmail.com>
2022-04-05 16:33:53 +0200

    Added loading

--- src/components/pages/blog.tsx
+++ src/components/pages/blog.tsx
@@ -24,6 +24,7 @@ export function Blog() {
     Array<File>
   >);
   const [imageURLs, setImageURLs] = useState([] as Array<Array<string>>);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -49,193 +50,209 @@ export function Blog() {
         <TitleInput state={state} setState={setState} />
       )}
 
-      {Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN && (
-        <div className="flex flex-col justify-center mb-8 bg-details-light h-24">
-          <div className="flex items-center w-full mb-2">
-            <div className="font-montserrat font-bold w-1/4 ml-4">Lati: </div>
-            <input
-              className="w-full h-10 px-2 bg-details-light border-base border ml-4 rounded font-montserrat mr-4"
-              type={"number"}
-              value={state.location.lat}
-              onChange={(event) => {
-                setState({
-                  ...state,
-                  location: {
-                    ...state.location,
-                    lat: parseFloat(event.target.value),
-                  },
-                });
-              }}
-            />
-          </div>
-          <div className="flex items-center w-full">
-            <div className="font-montserrat font-bold w-1/4 ml-4">Long: </div>
-            <input
-              className="w-full h-10 px-2 bg-details-light border-base border ml-4 rounded font-montserrat mr-4"
-              type={"number"}
-              value={state.location.long}
-              onChange={(event) => {
-                setState({
-                  ...state,
-                  location: {
-                    ...state.location,
-                    long: parseFloat(event.target.value),
-                  },
-                });
-              }}
-            />
-          </div>
-        </div>
-      )}
-
-      {state.posts.map((post, index) => {
-        return (
-          <div key={index} className="mt-8">
-            <div className="mt-2">
-              <div className="w-full">
-                <TextareaAutosize
-                  className="font-montserrat w-full bg-background resize-none px-2 py-2 rounded-lg text-sm"
-                  disabled={
-                    Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN
-                      ? false
-                      : true
-                  }
-                  value={post}
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN && (
+            <div className="flex flex-col justify-center mb-8 bg-details-light h-24">
+              <div className="flex items-center w-full mb-2">
+                <div className="font-montserrat font-bold w-1/4 ml-4">
+                  Lati:{" "}
+                </div>
+                <input
+                  className="w-full h-10 px-2 bg-details-light border-base border ml-4 rounded font-montserrat mr-4"
+                  type={"number"}
+                  value={state.location.lat}
                   onChange={(event) => {
-                    let posts = [...state.posts];
-                    posts[index] = event.target.value;
-                    setState({ ...state, posts });
+                    setState({
+                      ...state,
+                      location: {
+                        ...state.location,
+                        lat: parseFloat(event.target.value),
+                      },
+                    });
+                  }}
+                />
+              </div>
+              <div className="flex items-center w-full">
+                <div className="font-montserrat font-bold w-1/4 ml-4">
+                  Long:{" "}
+                </div>
+                <input
+                  className="w-full h-10 px-2 bg-details-light border-base border ml-4 rounded font-montserrat mr-4"
+                  type={"number"}
+                  value={state.location.long}
+                  onChange={(event) => {
+                    setState({
+                      ...state,
+                      location: {
+                        ...state.location,
+                        long: parseFloat(event.target.value),
+                      },
+                    });
                   }}
                 />
               </div>
             </div>
-            {uploadedFiles[index + 1] && (
+          )}
+
+          {state.posts.map((post, index) => {
+            return (
+              <div key={index} className="mt-8">
+                <div className="mt-2">
+                  <div className="w-full">
+                    <TextareaAutosize
+                      className="font-montserrat w-full bg-background resize-none px-2 py-2 rounded-lg text-sm"
+                      disabled={
+                        Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN
+                          ? false
+                          : true
+                      }
+                      value={post}
+                      onChange={(event) => {
+                        let posts = [...state.posts];
+                        posts[index] = event.target.value;
+                        setState({ ...state, posts });
+                      }}
+                    />
+                  </div>
+                </div>
+                {uploadedFiles[index + 1] && (
+                  <div className="flex w-full justify-center">
+                    <div>
+                      {uploadedFiles[index + 1].length > 0 && (
+                        <img
+                          className="max-h-56"
+                          src={URL.createObjectURL(uploadedFiles[index + 1][0])}
+                        />
+                      )}
+                    </div>
+                  </div>
+                )}
+                {imageURLs[index + 1] && (
+                  <div className="flex w-full justify-center">
+                    <div>
+                      {imageURLs[index + 1].length > 0 && (
+                        <img
+                          className="max-h-56"
+                          src={imageURLs[index + 1][0]}
+                        />
+                      )}
+                    </div>
+                  </div>
+                )}
+                {Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN && (
+                  <div>
+                    <input
+                      type={"file"}
+                      onChange={(event) => {
+                        let uploadedFilesCopy: Array<Array<File>> = [];
+                        uploadedFiles.map((uploadedFile) => {
+                          uploadedFilesCopy = [
+                            ...uploadedFilesCopy,
+                            uploadedFile,
+                          ];
+                        });
+                        const selectedFiles = Array.from(event.target.files);
+                        uploadedFilesCopy[index + 1] = selectedFiles;
+
+                        setUploadedFiles(uploadedFilesCopy);
+                      }}
+                    />
+                  </div>
+                )}
+              </div>
+            );
+          })}
+          <Title title="Gallery" />
+          <div className="mt-8">
+            {imageURLs[0] && (
               <div className="flex w-full justify-center">
                 <div>
-                  {uploadedFiles[index + 1].length > 0 && (
-                    <img
-                      className="max-h-56"
-                      src={URL.createObjectURL(uploadedFiles[index + 1][0])}
-                    />
-                  )}
+                  {imageURLs[0].map((imageUrl, index) => {
+                    return (
+                      <div key={index} className="mb-4 flex justify-center">
+                        <img className="max-h-60" src={imageUrl} />
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             )}
-            {imageURLs[index + 1] && (
+            {uploadedFiles[0] && (
               <div className="flex w-full justify-center">
                 <div>
-                  {imageURLs[index + 1].length > 0 && (
-                    <img className="max-h-56" src={imageURLs[index + 1][0]} />
-                  )}
+                  {uploadedFiles[0].map((uploadedFile, index) => {
+                    return (
+                      <div key={index} className="mb-4 flex justify-center">
+                        <img
+                          className="max-h-60"
+                          src={URL.createObjectURL(uploadedFile)}
+                        />
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             )}
             {Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN && (
-              <div>
-                <input
-                  type={"file"}
-                  onChange={(event) => {
-                    let uploadedFilesCopy: Array<Array<File>> = [];
-                    uploadedFiles.map((uploadedFile) => {
-                      uploadedFilesCopy = [...uploadedFilesCopy, uploadedFile];
-                    });
-                    const selectedFiles = Array.from(event.target.files);
-                    uploadedFilesCopy[index + 1] = selectedFiles;
+              <input
+                type={"file"}
+                multiple
+                onChange={(event) => {
+                  let uploadedFilesCopy: Array<Array<File>> = [];
+                  uploadedFiles.map((uploadedFile) => {
+                    uploadedFilesCopy = [...uploadedFilesCopy, uploadedFile];
+                  });
+                  const selectedFiles = Array.from(event.target.files);
+                  uploadedFilesCopy[0] = selectedFiles;
 
-                    setUploadedFiles(uploadedFilesCopy);
-                  }}
-                />
-              </div>
+                  setUploadedFiles(uploadedFilesCopy);
+                }}
+              />
             )}
           </div>
-        );
-      })}
-      <Title title="Gallery" />
-      <div className="mt-8">
-        {imageURLs[0] && (
-          <div className="flex w-full justify-center">
-            <div>
-              {imageURLs[0].map((imageUrl, index) => {
-                return (
-                  <div key={index} className="mb-4 flex justify-center">
-                    <img className="max-h-60" src={imageUrl} />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-        {uploadedFiles[0] && (
-          <div className="flex w-full justify-center">
-            <div>
-              {uploadedFiles[0].map((uploadedFile, index) => {
-                return (
-                  <div key={index} className="mb-4 flex justify-center">
-                    <img
-                      className="max-h-60"
-                      src={URL.createObjectURL(uploadedFile)}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-        {Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN && (
-          <input
-            type={"file"}
-            multiple
-            onChange={(event) => {
-              let uploadedFilesCopy: Array<Array<File>> = [];
-              uploadedFiles.map((uploadedFile) => {
-                uploadedFilesCopy = [...uploadedFilesCopy, uploadedFile];
-              });
-              const selectedFiles = Array.from(event.target.files);
-              uploadedFilesCopy[0] = selectedFiles;
-
-              setUploadedFiles(uploadedFilesCopy);
-            }}
-          />
-        )}
-      </div>
-      {Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN && (
-        <div className="flex justify-end mt-8">
-          <button
-            className="border border-base bg-highlights hover:bg-details-light w-1/3 mr-8 h-12 mb-4 rounded-xl font-montserrat"
-            onClick={() => {
-              setState({
-                ...state,
-                posts: [...state.posts, "Text here"],
-              });
-              setUploadedFiles([...uploadedFiles, []]);
-            }}
-          >
-            Add Chapter
-          </button>
-          <button
-            className="border border-base bg-highlights hover:bg-details-light w-1/2 h-12 mb-4 rounded-xl font-montserrat"
-            onClick={() => {
-              imageURLs;
-
-              CreateBlog({
-                id: state.id,
-                location: state.location,
-                posts: state.posts,
-                title: state.title,
-                date: GetCurrentDate(),
-              }).then((id) => {
-                uploadedFiles.map((uploadedFile, index) => {
-                  UploadImages({
-                    id: id,
-                    files: uploadedFile,
-                    chapter: index,
+          {Username === process.env.REACT_APP_AUTH_EMAIL_ADMIN && (
+            <div className="flex justify-end mt-8">
+              <button
+                className="border border-base bg-highlights hover:bg-details-light w-1/3 mr-8 h-12 mb-4 rounded-xl font-montserrat"
+                onClick={() => {
+                  setState({
+                    ...state,
+                    posts: [...state.posts, "Text here"],
                   });
-                });
-              });
-            }}
-          >
-            Submit
-          </button>
+                  setUploadedFiles([...uploadedFiles, []]);
+                }}
+              >
+                Add Chapter
+              </button>
+              <button
+                className="border border-base bg-highlights hover:bg-details-light w-1/2 h-12 mb-4 rounded-xl font-montserrat"
+                onClick={() => {
+                  imageURLs;
+
+                  CreateBlog({
+                    id: state.id,
+                    location: state.location,
+                    posts: state.posts,
+                    title: state.title,
+                    date: GetCurrentDate(),
+                  }).then((id) => {
+                    uploadedFiles.map((uploadedFile, index) => {
+                      UploadImages({
+                        id: id,
+                        files: uploadedFile,
+                        chapter: index,
+                      });
+                    });
+                  });
+                }}
+              >
+                Submit
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
